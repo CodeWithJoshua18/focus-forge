d91242b3debb68bb5aca3b6578695bbb4431e4fd
CodeWithJoshua18 <generaljosh5311@gmail.com>
2026-06-03 15:38:32 +0300

    Added calculating duration method

--- js/tracking/session.js
+++ js/tracking/session.js
@@ -3,8 +3,15 @@ function Session(activityName, startTime, endTime){
     this.activityName = activityName;
     this.startTime = startTime;
     this.endTime = endTime;
-    this.duration = startTime + endTime;
+    
+    this.totalDuration = () => {
+        return this.endTime - this.startTime;
+    }
+
+    // function to calculate total duration
+    
 }
 
 // creating a session object
 let s001 = new Session('Learn javascript', 1000, 1400);
+console.log(s001.totalDuration());

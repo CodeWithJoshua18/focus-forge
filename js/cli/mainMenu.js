3b42883d422ec668ec5cc661ce9a892eaf2d81c3
CodeWithJoshua18 <generaljosh5311@gmail.com>
2026-09-01 21:40:49 +0300

    Add displayMainMenu function: create main menu for user interaction

---
+++ js/cli/mainMenu.js
@@ -0,0 +1,13 @@
+export function displayMainMenu() {
+    return `
+=================================
+Welcome to Focus Forge
+=================================
+
+1. Create project
+2. List projects
+3. Create task
+4. List tasks
+5. Exit
+`;
+}

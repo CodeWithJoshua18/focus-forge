c61015cf517ffd067150028cacc127ba4b3b18b3
CodeWithJoshua18 <generaljosh5311@gmail.com>
2026-09-07 10:33:38 +0300

    Enhance route function: prompt for project details when adding a project

--- js/router.js
+++ js/router.js
@@ -1,12 +1,13 @@
 import { handleAddProject, handleListProjects } from "./projects/projectController.js";
 import { handleAddTask, handleListTasks } from "./tasks/taskController.js";
 import { exit } from "./cli/exit.js";
+import { promptProject } from "./cli/projectPrompt.js";
 
 // function to map user choice to appropriate controller
-export function route(choice) {
+export async function route(choice) {
     switch (choice) {
         case "1":
-            handleAddProject();
+            handleAddProject(await promptProject());
             break;
         case "2":
             handleListProjects();

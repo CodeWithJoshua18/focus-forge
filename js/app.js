adaa885b5ea4ffac63d12ca89c3e2002fb36ac69
CodeWithJoshua18 <generaljosh5311@gmail.com>
2026-09-07 10:33:08 +0300

    Enhance startApp function: load projects and set them before main loop

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 import { displayMainMenu } from "./cli/mainMenu.js";
 import { prompt } from "./cli/prompts.js";
 import { route } from "./router.js";
+import { loadProjects } from "./projects/projectFileStorage.js";
+import { setProjects } from "./projects/projectManager.js";
 
 async function startApp(){
+    const projects = loadProjects();
+
+    setProjects(projects);
+
     while (true){
         console.log(displayMainMenu());
 
         const choice = await prompt();
 
-        route(choice);
+       await route(choice);
     };
 }
 

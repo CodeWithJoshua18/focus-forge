7b1756eebcf893a0de97d46290fb072730ffd49c
CodeWithJoshua18 <generaljosh5311@gmail.com>
2026-09-07 13:31:49 +0300

    Enhance app.js to load and set task data during application startup

--- js/app.js
+++ js/app.js
@@ -3,11 +3,17 @@ import { prompt } from "./cli/prompts.js";
 import { route } from "./router.js";
 import { loadProjects } from "./projects/projectFileStorage.js";
 import { setProjects } from "./projects/projectManager.js";
+import { loadTasks } from "./tasks/taskFileStorage.js";
+import { setTasks } from "./tasks/taskManager.js";
 
 async function startApp(){
+    // hydrate project data
     const projects = loadProjects();
-
     setProjects(projects);
+
+    // hydrate task data
+    const tasks = loadTasks();
+    setTasks(tasks);
 
     while (true){
         console.log(displayMainMenu());

0503054fb269851d9ba36eaacb5ae36746ac6125
CodeWithJoshua18 <generaljosh5311@gmail.com>
2026-09-09 09:58:22 +0300

    Add: enhance routing logic to render project and task lists properly

--- js/router.js
+++ js/router.js
@@ -3,6 +3,8 @@ import { handleAddTask, handleListTasks } from "./tasks/taskController.js";
 import { exit } from "./cli/exit.js";
 import { promptProject } from "./cli/projectPrompt.js";
 import { promptTask } from "./cli/taskPrompt.js";
+import { renderProjects } from "./projects/projectView.js";
+import { log } from "node:console";
 
 // function to map user choice to appropriate controller
 export async function route(choice) {
@@ -11,13 +13,15 @@ export async function route(choice) {
             handleAddProject(await promptProject());
             break;
         case "2":
-            console.log(handleListProjects());
+            const projects = handleListProjects();
+            console.log(renderProjects(projects));
             break;
         case "3":
             handleAddTask(await promptTask());
             break;
         case "4":
-            console.log(handleListTasks());
+            const tasks = handleListTasks();
+            console.log(renderTasks(tasks));
             break;
         case "5":
             exit();

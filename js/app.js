9e76386661279ec5f6ce47f0747b265e6efbbc2b
CodeWithJoshua18 <generaljosh5311@gmail.com>
2026-09-15 22:35:55 +0300

    refactor: update project and task loading to use persistence implementations

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
 import { displayMainMenu } from "./cli/mainMenu.js";
 import { prompt } from "./cli/prompts.js";
 import { route } from "./router.js";
-import { loadProjects } from "./projects/projectFileStorage.js";
+
+import { projectFileStorage } from "./projects/projectFileStorage.js";
+import { taskFileStorage } from "./tasks/taskFileStorage.js";
+
+import { createProjectPersistence } from "./persistence/projectPersistence.js";
+import { createTaskPersistence } from "./persistence/taskPersistence.js";
+
 import { setProjects } from "./projects/projectManager.js";
-import { loadTasks } from "./tasks/taskFileStorage.js";
 import { setTasks } from "./tasks/taskManager.js";
 
 async function startApp(){
+
+    // choose persistence implementations
+    const projectPersistence = createProjectPersistence(projectFileStorage);
+
+    const taskPersistence = createTaskPersistence(taskFileStorage);
+
     // hydrate project data
-    const projects = loadProjects();
+    const projects = projectPersistence.load();
     setProjects(projects);
 
     // hydrate task data
-    const tasks = loadTasks();
+    const tasks = taskPersistence.load();
     setTasks(tasks);
 
     while (true){
@@ -20,8 +31,8 @@ async function startApp(){
 
         const choice = await prompt();
 
-       await route(choice);
-    };
+        await route(choice);
+    }
 }
 
 startApp();

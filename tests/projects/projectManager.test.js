6ec826a0551afc5cd2e9c6736a15f62213638526
CodeWithJoshua18 <generaljosh5311@gmail.com>
2026-09-24 14:56:09 +0300

    test: enhance deleteProject tests with detailed assertions and scenarios

--- tests/projects/projectManager.test.js
+++ tests/projects/projectManager.test.js
@@ -43,8 +43,40 @@ describe("deleteProject", () => {
         const result = deleteProject("1");
 
         // Assert
-        expect(result).toBe(false);
+        expect(result).toBe("Project_Has_Incomplete_Tasks");
         expect(getProjectById("1")).not.toBeNull();
+    });
+
+    test("returns not found result if there is no matching project ID", () => {
+        // Arrange
+        setProjects([]);
+        setTasks([]);
+
+        // Act
+        const notFoundProject = deleteProject("2");
+
+        // Assert
+        expect(notFoundProject).toBe("Project_Not_Found");
+    });
+
+    test("return message success once project is deleted successfully", () => {
+        // Arrange
+        setProjects([]);
+        setTasks([]);
+
+        const project = createProject({
+            id: "1",
+            name: "Focus-Forge"
+        });
+
+        addProject(project);
+
+        // Act
+        const successfullDeletion = deleteProject("1");
+
+        // Assert
+        expect(successfullDeletion).toBe("Project_Deleted");
+        expect(getProjectById("1")).toBeNull();
     });
 
 });
